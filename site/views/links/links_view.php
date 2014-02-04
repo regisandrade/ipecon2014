4bbbfcb46252be44e8d0bdd08bd0c187e3ffc988
Regis Andrade <regisandrade@gmail.com>
2014-02-04 21:57:17 -0200

    corrigindo a lista de links

--- site/views/links/links_view.php
+++ site/views/links/links_view.php
@@ -1,7 +1,7 @@
 <div id="pagina-interna">
 	<div class="internaCtrl">
 		<div class="titulo">Links</div>
-			<div class="texto">
+			<div class="links">
 				<ul>
 				<?php foreach($links as $e) {
 					echo "<li><a href=\"http://".$e->Link."\" target=\"_blank\">".$e->Descricao."</a></li>";
